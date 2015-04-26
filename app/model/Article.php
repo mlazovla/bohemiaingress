57d72021c4c4fcaf532947458f71fb0c764efd86
mlazovla <mlazovla@gmail.com>
2015-04-26 20:53:53 +0200

    Přidán presenter a šablona pro Article

--- app/model/Article.php
+++ app/model/Article.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Model;
-use Nette\Database\Context;
 
 /**
  * Persistent object Article. 
@@ -9,10 +8,10 @@ use Nette\Database\Context;
 class Article extends \Nette\Database\Table\Selection {
     private $db;
     private $table = "article";
-    
-    public function __construct(\Nette\Database\Context $database) {
-        parent::__construct($database, $this->table);
-        $this->db = $database;
-    }
+
+	public function __construct(\Nette\Database\Context $database) {
+		parent::__construct($database, $database->getConventions(), $this->table);
+		$this->db = $database;
+	}
     
 }
---
+++ app/presenters/ArticlePresenter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+	App\Model;
+
+use App\Model\Article;
+
+
+/**
+ * Article presenter.
+ */
+class ArticlePresenter extends BasePresenter
+{
+
+	/**
+	 * @var Article
+	 * @inject
+	 */
+	public $article;
+
+	public function renderShow($id)
+	{
+		$this->template->article = $this->article->get($id);
+	}
+
+}
